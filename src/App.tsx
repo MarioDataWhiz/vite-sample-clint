412070db7874c4dc64f004a6ae6b2ea8b053ac73
MarioTorres <mario.torres42@utexas.edu>
2025-04-04 11:51:47 -0500

    Update text with Emoji

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Clint + Code</h1>
+      <h1>Mario + Code = ❤️📈 </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
